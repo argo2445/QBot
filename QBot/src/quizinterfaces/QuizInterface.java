10f37eea70dcb8b201d11b18b5dc8881af0fccd3
Trazone <timon.walkenhorst@tu-dortmund.de>
2017-09-30 14:56:33 +0200

    Quizinterface methods added

--- QBot/src/quizinterfaces/QuizInterface.java
+++ QBot/src/quizinterfaces/QuizInterface.java
@@ -2,4 +2,18 @@ package quizinterfaces;
 
 public interface QuizInterface {
 	
+	public int createGame();
+	
+	public void addPlayer(String name, int gameID);
+	
+	public void startGame(int gameID, int rounds);
+	
+	public Question fetchQuestion(int gameID);
+	
+	public boolean enterAnswer(int gameID, String name, Answer answer);
+	
+	public List<String> retrieveScore(int gameID);
+	
+	public void endGame(int gameID);
+	
 }
