5601faeac1c97f64d3ab9b9f99b0d71a1a1afe14
dominik <argozwoviervierfuenf@gmail.com>
2017-10-01 06:18:32 +0200

    Organized Imports

--- QBot/src/quiz/QuizController.java
+++ QBot/src/quiz/QuizController.java
@@ -8,7 +8,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import quizinterfaces.AnswerInterface;
 import quizinterfaces.QuestionInterface;
 import quizinterfaces.QuizInterface;
 
